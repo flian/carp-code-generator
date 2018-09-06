c47856e1752850b8f3190477b5c55c2418038089
Foy Lian <ljs2342003@gmail.com>
2018-09-06 16:37:59 +0800

    refactor

--- code-generator-core/src/main/java/org/lotus/carp/generator/core/JpaGenerator.java
+++ code-generator-core/src/main/java/org/lotus/carp/generator/core/JpaGenerator.java
@@ -1,25 +1,19 @@
 package org.lotus.carp.generator.core;
 
-import com.google.common.base.CaseFormat;
 import freemarker.template.Template;
 import org.lotus.carp.generator.base.config.Config;
-import org.lotus.carp.generator.base.table.Column;
 import org.lotus.carp.generator.base.table.Databse;
 import org.lotus.carp.generator.base.table.Table;
-import org.lotus.carp.generator.core.dto.EntityAttributeDto;
-import org.lotus.carp.generator.core.dto.EntityDto;
+import org.lotus.carp.generator.core.utils.GeneratorUtils;
 
 import java.io.*;
-import java.math.BigDecimal;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.*;
-import java.util.stream.Collectors;
-
-import static java.sql.Types.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -28,6 +22,7 @@ import static java.sql.Types.*;
  * Date: 7/26/2018
  * Time: 9:59 AM
  */
+@Deprecated
 public class JpaGenerator {
     private static JpaConfig jpaConfig;
     private static List<Table> tableInfo = (new Databse()).gatherInfo();
@@ -53,16 +48,6 @@ public class JpaGenerator {
         }
     }
 
-    private void makeSureFolderExist(Path out) {
-        if (!Files.exists(out)) {
-            try {
-                Files.createDirectories(out);
-            } catch (IOException e) {
-                e.printStackTrace();
-                return;
-            }
-        }
-    }
 
     public void rendEntityAndRepository() {
         rendEntityAndRepository(false);
@@ -73,7 +58,7 @@ public class JpaGenerator {
         Template repositoryTemplate = Freemarker.template(jpaConfig.getRepositoryTemplateName());
         Template serviceTemplate = Freemarker.template(jpaConfig.getServiceTemplateName());
         Template serviceImplTemplate = Freemarker.template(jpaConfig.getServiceImplTemplateName());
-        colectionEntityList().stream().forEach(item -> {
+        GeneratorUtils.collectEntityList(tableInfo,jpaConfig,date,time).stream().forEach(item -> {
             Writer entityWriter = null;
             Writer repositoryWriter = null;
             Writer serviceWriter = null;
@@ -90,10 +75,10 @@ public class JpaGenerator {
                     Path repositoryPath = Paths.get(repositoryOutputDir);
                     Path servicePath = Paths.get(serviceOutputDir);
                     Path serviceImplPath = Paths.get(serviceImplOutputDir);
-                    makeSureFolderExist(entityOutPath);
-                    makeSureFolderExist(repositoryPath);
-                    makeSureFolderExist(servicePath);
-                    makeSureFolderExist(serviceImplPath);
+                    GeneratorUtils.makeSureFolderExist(entityOutPath);
+                    GeneratorUtils.makeSureFolderExist(repositoryPath);
+                    GeneratorUtils.makeSureFolderExist(servicePath);
+                    GeneratorUtils.makeSureFolderExist(serviceImplPath);
                     try {
                         entityWriter = new OutputStreamWriter(new FileOutputStream(entityOutputDir + item.getEntityFileName()));
                         repositoryWriter = new OutputStreamWriter(new FileOutputStream(repositoryOutputDir + item.getRepositoryFileName()));
@@ -111,84 +96,5 @@ public class JpaGenerator {
             Freemarker.render(serviceTemplate, params, serviceWriter);
             Freemarker.render(serviceImplTemplate, params, serviceImplWriter);
         });
-    }
-
-    private List<EntityDto> colectionEntityList() {
-        final List<EntityDto> result = new ArrayList<>();
-        tableInfo.forEach(table -> {
-            EntityDto entityDto = new EntityDto();
-            entityDto.setAuthor(jpaConfig.getAuthor());
-            entityDto.setDate(date);
-            entityDto.setTime(time);
-            entityDto.setEntitySufix(jpaConfig.getEntitySufix());
-            entityDto.setRepositorySufix(jpaConfig.getRepositorySufix());
-            entityDto.setTableName(table.getName());
-            entityDto.setUuid("" + uuid() + "L");
-            entityDto.setEntityPackage(jpaConfig.getEntityPackage());
-            entityDto.setRepositoryPackage(jpaConfig.getRepositoryPackage());
-            entityDto.setServicePackage(jpaConfig.getServicePackage());
-            entityDto.setServiceImplPackage(jpaConfig.getServiceImplPackage());
-            entityDto.setServiceSufix(jpaConfig.getServiceSufix());
-            entityDto.setServiceImplSufix(jpaConfig.getServiceImplSufix());
-
-            String className = toClassName(table.getName());
-            entityDto.setClassName(className);
-            entityDto.setComment(table.getComment());
-            entityDto.setPkType(map2JavaType(table.firstPkColumn()));
-            entityDto.setAttributes(table.getColumns().stream().map(column -> {
-                EntityAttributeDto entityAttributeDto = new EntityAttributeDto();
-                entityAttributeDto.setAutoincrement(column.isAutoincrement());
-                entityAttributeDto.setPk(table.isColAutoincrementPrimaryKey(column.getName()));
-                entityAttributeDto.setColumnName(column.getName());
-                entityAttributeDto.setPropertyName(toPropertyName(column.getName()));
-                entityAttributeDto.setPropertyType(map2JavaType(column));
-                entityAttributeDto.setComment(column.getComment());
-                return entityAttributeDto;
-            }).collect(Collectors.toList()));
-            result.add(entityDto);
-        });
-        return result;
-    }
-
-
-    private Long uuid() {
-        return UUID.randomUUID().getMostSignificantBits();
-    }
-
-    private String map2JavaType(Column column) {
-        switch (column.getDataType()) {
-            case BIT:
-            case TINYINT:
-            case SMALLINT:
-            case INTEGER:
-                return Integer.class.getSimpleName();
-            case BIGINT:
-                return Long.class.getSimpleName();
-            case FLOAT:
-            case REAL:
-            case DOUBLE:
-            case NUMERIC:
-                return Double.class.getSimpleName();
-            case DECIMAL:
-                return BigDecimal.class.getSimpleName();
-            case DATE:
-            case TIME:
-            case TIMESTAMP:
-                return Date.class.getSimpleName();
-            case CHAR:
-            case VARCHAR:
-            case LONGVARCHAR:
-                return String.class.getSimpleName();
-            default:
-                return String.class.getSimpleName();
-        }
-    }
-
-    private String toClassName(String tableName) {
-        return CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, tableName.toLowerCase());
-    }
-
-    private String toPropertyName(String columnName) {
-        return CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, columnName.toLowerCase());
     }
 }
